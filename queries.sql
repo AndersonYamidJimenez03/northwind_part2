49c2753d35f85d883dbe4606773a1dfd00ae9ecc
ander <64057032+AndersonYamidJimenez03@users.noreply.github.com>
2024-10-09 22:39:10 -0400

    commit for functions

--- queries.sql
+++ queries.sql
@@ -41,24 +41,109 @@ CREATE INDEX productNameIndex
 ON products (productname);
 
 /*
-    Transactions
-*/
+    Transaction for practice, and see the rollback
+    keyword in action in the database.
 
-SELECT * FROM products;
+*/
 
 BEGIN TRANSACTION;
 UPDATE products
 SET unitprice = 19
 WHERE productId = 1;
 
+-- Here, I check the update transaction
 SELECT productId, unitprice
 FROM products;
 
 -- COMMIT in case the save the changes
 ROLLBACK TRANSACTION;
-
-
+/*
+    At the end, i can see that with the ROLLBACK,
+    the database continue the same.
+*/
 SELECT productId, unitprice
 FROM products;
+
+
+-- FUNCTIONS
+
+/*
+    Las funciones siempre deben devolver un valor, ya sea escalar o una tabla.
+    Las funciones no pueden realizar modificationes a la 
+    base de datos, por lo tanto no pueden usar ciertos
+    clausulas (update, delete, insert)
+*/
+
+-- Function que retorna un escalar
+
+CREATE OR REPLACE FUNCTION Discount(
+    percentage DECIMAL(5,2),
+    value INT
+)
+RETURNS DECIMAL(10,2)
+AS
+$$
+BEGIN
+    RETURN (percentage * value);
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT Discount(0.50, 1000) AS TotalDiscount;
+
+-- Function que retorna una tabla
+
+CREATE OR REPLACE FUNCTION filterCustomerCountry(
+    country_ varchar(20)
+)
+RETURNS TABLE(
+    customerId varchar(10),
+    companyname varchar(40),
+    contactname varchar(30),
+    contacttitle varchar(30),
+    city varchar(20),
+    region varchar(20),
+    postalcode varchar(15),
+    country varchar(20),
+    phone varchar(20),
+    fax varchar(20)
+)
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT *
+        FROM customers
+        WHERE customers.country = country_;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM filterCustomerCountry('UK');
+
+
+-- Function que returna tabla
+
+SELECT * FROM orders;
+DROP FUNCTION IF EXISTS ordersSummarizeByCountry(varchar(20));
+CREATE OR REPLACE FUNCTION ordersSummarizeByCountry(
+    country_ varchar(20)
+)
+RETURNS TABLE(
+    shipcountry varchar(50),
+    shipcity varchar(50),
+    order_qty INT
+)
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT o.shipcountry, o.shipcity, COUNT(*)::INT AS order_qty
+        FROM orders o
+        WHERE o.shipcountry = country_
+        GROUP BY o.shipcountry, o.shipcity
+        ORDER BY order_qty DESC;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM ordersSummarizeByCountry('France');
 
 
